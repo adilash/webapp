7e73b08e21a28c4ee23456b1bbd37a0d34a1f4da
adilash <adilabdulsamad@hotmail.com>
2016-09-29 15:39:19 +0530

    econd Commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,12 +14,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/df.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'df.png'));
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
